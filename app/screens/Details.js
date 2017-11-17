1045b008f483d660bf3f0738ea711d88e9cd4c89
kinjo yuji <kinjoyuji@gmail.com>
2017-11-18 02:34:17 +0900

    詳細ページのヘッダーを実装

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from '../components/UserDetails';
+import colors from '../config/colors';
 
 export default class Details extends Component {
   render(){
+    const contact = this.props.navigation.state.params;
+
     return(
-      <View style={ { flex: 1, alignItems: 'center', justifyContent: 'center' } }>
-        <Text>Details Screen</Text>
+      <View style={ { backgroundColor: colors.background } }>
+        <Header {...contact}/>
       </View>
     );
   }
