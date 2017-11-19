f810b0c5821f42f2efcd03ba4832cded293e877c
kinjo yuji <kinjoyuji@gmail.com>
2017-11-19 12:15:50 +0900

    リファクタ

---
+++ app/components/UserDetails/Row.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import { View, TouchableOpacity, Text, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import colors from '../../config/colors';
+import styles, { ICON_SIZE } from './styles';
+
+const Row = ({ label, body, actions = [] }) => {
+  return (
+    <View style={styles.actionRow}>
+      <View style={styles.actionInfo}>
+        <Text style={styles.actionLabel}>{label}</Text>
+        <Text style={styles.actionBody}>{body}</Text>
+      </View>
+      <View style={styles.actionIcons}>
+        {actions.map((action, index) => (
+          <TouchableOpacity
+            onPress={action.onPress}
+            ket={index}
+          >
+            <Icon
+              style={styles.actionIcon}
+              color={colors.link}
+              size={ICON_SIZE}
+              name={Platform.OS === 'ios' ? action.iconIOS : action.iconAndroid}
+            />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+export default Row;
--- app/components/UserDetails/actions.js
+++ app/components/UserDetails/actions.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { View, Platform, TouchableOpacity, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
+import React, { PropTypes } from 'react';
+import { View } from 'react-native';
 
-import styles, { ICON_SIZE } from './styles';
-import colors from '../../config/colors';
+import styles from './styles';
 import { toPhoneNumber } from '../../helpers/string';
+import Row from './Row';
 
-const Actions = ({ email }) => {
-  return (
-    <View style={styles.actionContainer}>
-      <View style={styles.actionRow}>
-        <View style={styles.actionInfo}>
-          <Text style={styles.actionLabel}>email</Text>
-          <Text style={styles.actionBody}>{email}</Text>
-          <View style={styles.actionIcons}>
-            <TouchableOpacity
-              onPress={() => null}
-            >
-            <Icon
-              style={styles.actionIcon}
-              color={colors.link}
-              size={ICON_SIZE}
-              name={Platform.OS === 'ios' ? 'ios-mail' : 'md-mail'}
-            />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
+const Actions = ({ email, phone, cell }) => (
+  <View style={styles.actionContainer}>
+    <Row
+      label="email"
+      body={email}
+      actions={[
+        { iconIOS: 'ios-mail', iconAndroid: 'md-mail' },
+      ]}
+    />
+    <Row
+      label="mobile"
+      body={toPhoneNumber(cell)}
+      actions={[
+        { iconIOS: 'ios-call', iconAndroid: 'md-call' },
+        { iconIOS: 'ios-text', iconAndroid: 'md-text' },
+      ]}
+    />
+    <Row
+      label="home"
+      body={toPhoneNumber(phone)}
+      actions={[
+        { iconIOS: 'ios-call', iconAndroid: 'md-call' },
+      ]}
+    />
+  </View>
+);
+
+// Actions.propTypes = {
+//   email: PropTypes.string,
+//   phone: PropTypes.string,
+//   cell: PropTypes.string,
+// };
 
 export default Actions;
