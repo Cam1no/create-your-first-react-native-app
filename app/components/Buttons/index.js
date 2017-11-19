db8bbd7ea5845645bf507200d0de65445f4e05c0
kinjo yuji <kinjoyuji@gmail.com>
2017-11-19 18:23:35 +0900

    編集ボタン追記

---
+++ app/components/Buttons/index.js
@@ -0,0 +1,8 @@
+import PrimaryButton from './PrimaryButton';
+import styles from './styles';
+
+
+export {
+  PrimaryButton,
+  styles,
+}
--- app/screens/Me.js
+++ app/screens/Me.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Header, Actions, Info } from '../components/UserDetails';
+import { PrimaryButton } from '../components/Buttons';
+
 import colors from '../config/colors';
 import { me } from '../config/data';
 
@@ -9,6 +11,10 @@ export default class Me extends Component {
     return(
       <ScrollView style={ { backgroundColor: colors.background } }>
         <Header {...me} />
+        <PrimaryButton
+          label={'Edit Profile'}
+          onPress={() => null}
+        />
         <Actions {...me} />
         <Info {...me} />
       </ScrollView>
